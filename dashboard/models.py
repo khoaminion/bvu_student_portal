8bee8517fc4f4b96015616807033a7072125b29b
khoaminion <minionsdt@gmail.com>
2022-12-30 17:58:04 +0700

    done dictionary

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,12 +4,29 @@ from django.contrib.auth.models import User
 
 class Notes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    desc = models.TextField()
+    title = models.CharField('Tiêu đề',max_length=200)
+    desc = models.TextField('Chi tiết ghi chú')
 
     class Meta:
         verbose_name = 'notes'
         verbose_name_plural = 'notes'
 
+    def __str__(self):
+        return self.title
+
+class Homework(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    subject = models.CharField('Môn học',max_length=80)
+    title = models.CharField('Tiêu đề',max_length=100)
+    desc = models.TextField('Chi tiết bài tập')
+    due = models.DateTimeField('Hạn chót')
+    is_finished = models.BooleanField('Đã hoàn thành ?',default=False)
+    def __str__(self):
+        return self.title
+
+class Todo(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField('Tiêu đề',max_length=100)
+    is_finished = models.BooleanField('Đã hoàn thành ?',default=False)
     def __str__(self):
         return self.title
