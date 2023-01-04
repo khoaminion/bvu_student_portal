64815188cd43664aac042446f7880c9beca0be65
khoaminion <minionsdt@gmail.com>
2023-01-04 17:35:36 +0700

    railway

--- bvu_student_portal/settings.py
+++ bvu_student_portal/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
@@ -133,7 +133,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 LOGIN_REDIRECT_URL ='home'
 LOGIN_URL = 'login'
