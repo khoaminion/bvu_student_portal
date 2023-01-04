14f45f7a021f1a98564e3b327c343058c2220eb7
khoaminion <minionsdt@gmail.com>
2023-01-04 11:27:40 +0700

    change db to sqlite

--- bvu_student_portal/settings.py
+++ bvu_student_portal/settings.py
@@ -77,12 +77,9 @@ WSGI_APPLICATION = 'bvu_student_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bvustudentportaldb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'bvustportaldb',
+                 
     }
 }
 
